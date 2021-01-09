57d1f7816d92d4c28380eaa63d4790ddd36be716
outstandingboy <outstanding1301@gmail.com>
2021-01-09 18:27:50 +0900

    feat: 포스트, 이미지 템플릿 생성 스크립트 개발

---
+++ post.py
@@ -0,0 +1,56 @@
+from datetime import datetime
+import re
+import os
+
+def main():
+    print("====================================")
+    print(" ✏ post writer by outstandingboy")
+    print("====================================")
+    today = datetime.today()
+
+    category = input("category: ")
+    while category == "":
+        category = input("category: ")
+
+    datePattern = re.compile('[0-9]{4}-[0-9]{2}-[0-9]{2}')
+    date = "%04d-%02d-%02d"%(today.year, today.month, today.day)
+    _date = input("date (default: %s): "%date)
+
+    while not (_date == "" or datePattern.match(_date)):
+        _date = input("date (default: %s): "%date)
+
+    if _date != "":
+        date = _date
+
+    filename = input("filename: ")
+    while filename == "":
+        filename = input("filename: ")
+
+    filename = filename.replace(" ", "-")
+    if filename.endswith(".md"):
+        filename = filename[0:-3]
+
+    print("%s/%s-%s.md"%(category, date, filename))
+
+    postDir = "_posts/%s"%(category)
+    postPath = "_posts/%s/%s-%s.md"%(category, date, filename)
+    imgPath = "imgs/%s/%s-%s"%(category, date, filename)
+
+    os.makedirs(postDir, exist_ok=True)
+    post = open(postPath, "w")
+    post.write("---\n")
+    post.write('title: "TITLE"\n')
+    post.write("category: %s\n"%category)
+    post.write("tags: [%s]\n"%category)
+    post.write("comments: true\n")
+    post.write("---\n")
+    post.close()
+    print("[📄] create file %s"%postPath)
+
+    os.makedirs(imgPath, exist_ok=True)
+    print("[📂] create directory %s"%imgPath)
+
+    print("[😏] Done")
+
+if __name__ == "__main__":
+    main()
